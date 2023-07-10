42d5afaf7d5209f848bf2dca07a68669728155c1
Christopher Lovell <avarensoldier@gmail.com>
2023-07-10 18:26:05 +0930

    Parser phase now only applies operations that end with new-lines. Solves false-positives such as: 	'PAst' being interpreted as 'pa'. 	'RAnge' being interpreted as 'ra' etc.

--- psvisdata/src/simulation.c
+++ psvisdata/src/simulation.c
@@ -9,25 +9,25 @@ t_sop get_operation(char *op)
 {
 	t_sop ret = ERR;
 
-	if (ft_strncmp(op, "pa", 2) == 0)
+	if (ft_strncmp(op, "pa\n", 3) == 0)
 		ret = PA;
-	else if (ft_strncmp(op, "pb", 2) == 0)
+	else if (ft_strncmp(op, "pb\n", 3) == 0)
 		ret = PB;
-	else if (ft_strncmp(op, "sa", 2) == 0)
+	else if (ft_strncmp(op, "sa\n", 3) == 0)
 		ret = SA;
-	else if (ft_strncmp(op, "sb", 2) == 0)
+	else if (ft_strncmp(op, "sb\n", 3) == 0)
 		ret = SB;
-	else if (ft_strncmp(op, "ss", 2) == 0)
+	else if (ft_strncmp(op, "ss\n", 3) == 0)
 		ret = SS;
-	else if (ft_strncmp(op, "rra", 3) == 0)
+	else if (ft_strncmp(op, "rra\n", 4) == 0)
 		ret = RRA;
-	else if (ft_strncmp(op, "ra", 2) == 0)
+	else if (ft_strncmp(op, "ra\n", 3) == 0)
 		ret = RA;
-	else if (ft_strncmp(op, "rrb", 3) == 0)
+	else if (ft_strncmp(op, "rrb\n", 4) == 0)
 		ret = RRB;
-	else if (ft_strncmp(op, "rb", 2) == 0)
+	else if (ft_strncmp(op, "rb\n", 3) == 0)
 		ret = RB;
-	else if (ft_strncmp(op, "rrr", 2) == 0)
+	else if (ft_strncmp(op, "rrr\n", 4) == 0)
 		ret = RRR;
 	return (ret);
 }
